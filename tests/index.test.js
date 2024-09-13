2472d7543eae5876cf3df81ea284c0deae4dcc9a
erick-sts <erick.sts.eo@gmail.com>
2024-09-13 20:13:01 -0300

    teste

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,31 +1,29 @@
 // tests/index.test.js
-const request = require('supertest');
-const app = require('../src/index');
+const request = require("supertest");
+const app = require("../src/index");
 
-describe('GET /api', () => {
-  it('should return Hello world!', async () => {
-    const res = await request(app).get('/api');
+describe("GET /api", () => {
+  it("should return Hello world!", async () => {
+    const res = await request(app).get("/api");
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello world!');
+    expect(res.text).toBe("Hello world!");
   });
 });
 
-describe('GET /api/test', () => {
-  it('should return a test message', async () => {
-    const res = await request(app).get('/api/test');
+describe("GET /api/test", () => {
+  it("should return a test message", async () => {
+    const res = await request(app).get("/api/test");
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('message', 'Endpoint de teste');
+    expect(res.body).toHaveProperty("message", "Endpoint de teste");
   });
 });
 
-/*
-describe('GET /api/test', () => {
-  it('should return a test message', async () => {
-    const res = await request(app).get('/api/test-2');
+describe("GET /api/test", () => {
+  it("should return a test message", async () => {
+    const res = await request(app).get("/api/test-2");
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('message', 'Endpoint de teste 2');
+    expect(res.body).toHaveProperty("message", "Endpoint de teste 2");
   });
 });
-*/
 
-jest.setTimeout(30000); 
+jest.setTimeout(30000);
